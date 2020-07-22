4d18631432e0cf50962fb22c020b345506fc096f
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-22 12:48:37 -0600

    Adds and passes publication year ttest

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,24 @@
 class Book
   attr_reader :title,
               :author_first_name,
-              :author_last_name
+              :author_last_name,
+              :publication_date
 
   def initialize(data)
     @title = data[:title]
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
+    @publication_date = data[:publication_date]
   end
-
 
   def author
     @author_first_name + " " + @author_last_name
-  end 
+  end
+
+  def publication_year
+    @publication_date[-4] +
+    @publication_date[-3] +
+    @publication_date[-2] +
+    @publication_date[-1]
+  end
 end
