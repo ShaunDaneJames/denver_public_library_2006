20fe0f6626c4d6b4a2ff944962872b49ded67747
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-22 12:54:59 -0600

    Adds and passes name method and teest

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,13 @@
 class Author
+  attr_reader :first_name,
+              :last_name
 
   def initialize(data)
-
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
   end
 
+  def name
+    @first_name + " " + @last_name
+  end 
 end
